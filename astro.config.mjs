1eb5a9db7f9a56700735ec211a5f7c945d93e2f3
Siddhesh Mangela <sidthecool007@gmail.com>
2022-08-18 16:04:14 +0530

    page created

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import image from '@astrojs/image';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://siddheshmangela.github.io',
@@ -8,5 +10,9 @@ export default defineConfig({
   vite: {
     // Example: Add custom vite plugins directly to your Astro project
     plugins: [tsconfigPaths()],
+    ssr: {
+      external: ['svgo'],
+    },
   },
+  integrations: [image()],
 });
